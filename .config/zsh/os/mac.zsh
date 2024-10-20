fe6c80124d88f9bee1c02d6ced2f5b527a50450b
Lucien Thany--Eynard <53664387+LTE-creator@users.noreply.github.com>
2024-10-20 12:08:56 +0530

    Initial commit

---
+++ .config/zsh/os/mac.zsh
@@ -0,0 +1,11 @@
+# Arch dependent brew stuff
+if [ "$(arch)" = "arm64" ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    eval "$(/usr/local/bin/brew shellenv)"
+fi
+if type brew &>/dev/null; then
+    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+    autoload -Uz compinit
+compinit
+fi
