257960bf8d06e06914e42faf486cc257ccc7f7e0
Lucien Thany--Eynard <53664387+LTE-creator@users.noreply.github.com>
2025-04-18 22:51:20 +0200

    yey

--- nix/flake.nix
+++ nix/flake.nix
@@ -135,5 +135,15 @@
           }
         ];
       };
+      # use "nixos", or your hostname as the name of the configuration
+      # it's a better practice than "default" shown in the video
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./configuration.nix
+          # inputs.home-manager.nixosModules.default
+        ];
+      };
+
     };
 }
